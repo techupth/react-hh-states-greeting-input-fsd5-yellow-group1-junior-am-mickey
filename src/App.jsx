6d1c8f49ba0c909400d78710cb80663e51fddb0d
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2023-12-21 14:55:18 +0700

    feat: add finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  let [greetingTypeMessage, setGreetingTypeMessage] = useState("");
+  function typeMessage(event) {
+    setGreetingTypeMessage(event.target.value);
+  }
+  function handleUpdate() {
+    setGreetingMessage(greetingTypeMessage);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          onChange={typeMessage}
+          value={greetingTypeMessage}
+          id="greeting-message"
+          type="text"
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleUpdate}>Update text</button>
       </div>
     </div>
   );
